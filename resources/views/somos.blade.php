f74cf00d8330fe82c1b814f3033af8ec29a87190
Quelfas <cristalmedia@gmail.com>
2016-03-25 10:21:08 -0430

    Add cont para Quienes Somos
    Se añade contenido para la seccion Quienes Somos

--- resources/views/somos.blade.php
+++ resources/views/somos.blade.php
@@ -10,7 +10,31 @@
 @endsection
 
 @section('content')
-  <p>
-    Bienvenido a Laravel
-  </p>
+
+  <div class="panel panel-info">
+    <div class="panel-heading">
+      <h3 class="panle-title">Quienes Somos</h3>
+    </div>
+
+    <div class="panel-body">
+      <div class="media">
+
+        <div class="media-left">
+          <a href="#">
+            <img class="media-object" src="{{asset('assets/img/front/hdcp.jpg')}}" alt="Asistencia a Discapacitados" width="64px" />
+          </a>
+        </div>
+
+        <div class="media-body">
+          Somos un equipo de jóvenes emprendedores que integra diversidad de
+           esfuerzos y trabaja con empeño por un fin común:  Ofrecer un espacio
+           de encuentro para personas con <span class="text-primary">discapacidad</span> y asi canalizar ayudas
+           puntuales para los que mas necesitan.
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
 @stop
