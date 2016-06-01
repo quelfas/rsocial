7697f9efddc39041016a39527932f758ac670828
Quelfas <cristalmedia@gmail.com>
2016-05-31 23:15:45 -0400

    Agregado Dropzone como herramienta de subida de imagenes
    DropzoneJS nueva herramienta para formulario de multiples imagenes
    Agregado a fileHandler.blade.php

--- resources/views/utility/fileHandler.blade.php
+++ resources/views/utility/fileHandler.blade.php
@@ -142,42 +142,130 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 
 
     </div>
+
+
     <div role="tabpanel" class="tab-pane" id="imagenes">
+      <script type="text/javascript" src="{{asset('assets/js/dropzone/dropzone.js')}}"></script>
+
       <h3>Galeria de Imagenes</h3>
+          <div id="actions" class="row">
 
-      <div id="drop_zone">Suelte los archivos aqui</div>
-      <output id="list"></output>
+        <div class="col-lg-7">
+          <!-- The fileinput-button span is used to style the file input field as button -->
+          <span class="btn btn-success fileinput-button">
+              <i class="glyphicon glyphicon-plus"></i>
+              <span>Add files...</span>
+          </span>
+          <button type="submit" class="btn btn-primary start">
+              <i class="glyphicon glyphicon-upload"></i>
+              <span>Start upload</span>
+          </button>
+          <button type="reset" class="btn btn-warning cancel">
+              <i class="glyphicon glyphicon-ban-circle"></i>
+              <span>Cancel upload</span>
+          </button>
+        </div>
 
-      <script>
-        function handleFileSelect(evt) {
-          evt.stopPropagation();
-          evt.preventDefault();
+        <div class="col-lg-5">
+          <!-- The global file processing state -->
+          <span class="fileupload-process">
+            <div id="total-progress" class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
+              <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
+            </div>
+          </span>
+        </div>
 
-          var files = evt.dataTransfer.files; // FileList object.
+      </div>
 
-          // files is a FileList of File objects. List some properties.
-          var output = [];
-          for (var i = 0, f; f = files[i]; i++) {
-            output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                        f.size, ' bytes, last modified: ',
-                        f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-                        '</li>');
-          }
-          document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-        }
 
-        function handleDragOver(evt) {
-          evt.stopPropagation();
-          evt.preventDefault();
-          evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-        }
+      <!-- HTML heavily inspired by http://blueimp.github.io/jQuery-File-Upload/ -->
+      <div class="table table-striped" class="files" id="previews">
 
-        // Setup the dnd listeners.
-        var dropZone = document.getElementById('drop_zone');
-        dropZone.addEventListener('dragover', handleDragOver, false);
-        dropZone.addEventListener('drop', handleFileSelect, false);
+      <div id="template" class="file-row">
+        <!-- This is used as the file preview template -->
+        <div>
+            <span class="preview"><img data-dz-thumbnail /></span>
+        </div>
+        <div>
+            <p class="name" data-dz-name></p>
+            <strong class="error text-danger" data-dz-errormessage></strong>
+        </div>
+        <div>
+            <p class="size" data-dz-size></p>
+            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
+              <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
+            </div>
+        </div>
+        <div>
+          <button class="btn btn-primary start">
+              <i class="glyphicon glyphicon-upload"></i>
+              <span>Start</span>
+          </button>
+          <button data-dz-remove class="btn btn-warning cancel">
+              <i class="glyphicon glyphicon-ban-circle"></i>
+              <span>Cancel</span>
+          </button>
+          <button data-dz-remove class="btn btn-danger delete">
+            <i class="glyphicon glyphicon-trash"></i>
+            <span>Delete</span>
+          </button>
+        </div>
+      </div>
+
+      </div>
+      <script type="text/javascript">
+      // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
+        var previewNode = document.querySelector("#template");
+        previewNode.id = "";
+        var previewTemplate = previewNode.parentNode.innerHTML;
+        previewNode.parentNode.removeChild(previewNode);
+
+        var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
+        url: "/imageup", // Set the url
+        thumbnailWidth: 80,
+        thumbnailHeight: 80,
+        parallelUploads: 20,
+        previewTemplate: previewTemplate,
+        autoQueue: false, // Make sure the files aren't queued until manually added
+        previewsContainer: "#previews", // Define the container to display the previews
+        clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+        });
+
+        myDropzone.on("addedfile", function(file) {
+        // Hookup the start button
+        file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file); };
+        });
+
+        // Update the total progress bar
+        myDropzone.on("totaluploadprogress", function(progress) {
+        document.querySelector("#total-progress .progress-bar").style.width = progress + "%";
+        });
+
+        myDropzone.on("sending", function(file) {
+        // Show the total progress bar when upload starts
+        document.querySelector("#total-progress").style.opacity = "1";
+        // And disable the start button
+        file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
+        });
+
+        // Hide the total progress bar when nothing's uploading anymore
+        myDropzone.on("queuecomplete", function(progress) {
+        document.querySelector("#total-progress").style.opacity = "0";
+        });
+
+        // Setup the buttons for all transfers
+        // The "add files" button doesn't need to be setup because the config
+        // `clickable` has already been specified.
+        document.querySelector("#actions .start").onclick = function() {
+        myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
+        };
+        document.querySelector("#actions .cancel").onclick = function() {
+        myDropzone.removeAllFiles(true);
+        };
       </script>
     </div>
+
+
     <div role="tabpanel" class="tab-pane" id="mensajes">...</div>
   </div>
 
