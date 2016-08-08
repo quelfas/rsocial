a0cc6be09d468ec4b09c6d436b00228c7ac370dd
Quelfas <cristalmedia@gmail.com>
2016-08-08 08:09:02 -0400

    Cambios en pag principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,41 @@
 
   <div class="row">
   <h1>Bienvenidos a Vidas Sobre Ruedas</h1>
-  <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
-  <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
-  <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
+  <div class="col-xs-6 col-sm-4">
+
+    <div class="panel panel-default">
+      <div class="panel-body">
+
+        <h4>.col-xs-6 .col-sm-4</h4>
+        <img src="{{ asset('assets/img/') }}/masculino.png" width="60" class="img-responsive img-circle" alt="Responsive image" align="left" style="margin:1px 0px">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      </div>
+    </div>
+
+  </div>
+  <div class="col-xs-6 col-sm-4">
+
+    <div class="panel panel-default">
+      <div class="panel-body">
+
+        <h4>.col-xs-6 .col-sm-4</h4>
+        <img src="{{ asset('assets/img/') }}/femenino.png" width="60" class="img-responsive img-circle" alt="Responsive image" align="left" style="margin:1px 0px">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      </div>
+    </div>
+
+  </div>
+  <div class="col-xs-6 col-sm-4">
+
+    <div class="panel panel-default">
+      <div class="panel-body">
+
+        <h4>.col-xs-6 .col-sm-4</h4>
+        <img src="{{ asset('assets/img/') }}/femenino.png" width="60" class="img-responsive img-circle" alt="Responsive image" align="left" style="margin:1px 0px">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      </div>
+    </div>
+
+  </div>
 </div>
 @stop
