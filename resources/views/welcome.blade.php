972798c2005e04bf260617af38ab585058829847
Quelfas <cristalmedia@gmail.com>
2016-08-12 08:02:32 -0400

    ajustes en la plantilla welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 
   <div class="row">
   <h1>Bienvenidos a Vidas Sobre Ruedas</h1>
-  <div class="col-xs-6 col-sm-4">
+  <div class="col-lg-4 col-md-4 col-sm-4">
 
     <div class="panel panel-default">
       <div class="panel-body">
@@ -19,7 +19,7 @@
     </div>
 
   </div>
-  <div class="col-xs-6 col-sm-4">
+  <div class="col-lg-4 col-md-4 col-sm-4">
 
     <div class="panel panel-default">
       <div class="panel-body">
@@ -31,22 +31,22 @@
     </div>
 
   </div>
-  <div class="col-xs-6 col-sm-4">
+  <div class="col-lg-4 col-md-4 col-sm-4">
 
     <div class="panel panel-default">
       <div class="panel-body">
 
-        <h4>.col-xs-6 .col-sm-4</h4>
+        <h4>.col-xs-6 col-sm-4</h4>
         <img src="{{ asset('assets/img/') }}/femenino.png" width="60" class="img-responsive img-circle" alt="Responsive image" align="left" style="margin:1px 0px">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
       </div>
     </div>
 
   </div>
-  
-  <div class="col-md-9">
+
+  <div class="col-xs-12">
       <img src="http://placehold.it/970x150" class="img-responsive">
   </div>
-  
+
 </div>
 @stop
